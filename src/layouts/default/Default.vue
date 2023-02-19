02b75091e6d4b2957d1cc476449ab685a4e45606
jhon janer <jhonjaner11@gmail.com>
2023-02-18 22:19:52 -0500

    first commit

---
+++ src/layouts/default/Default.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-app>
+    <v-app-bar color="primary" app dark>
+      <v-app-bar-nav-icon
+        variant="text"
+        @click.stop="drawer = !drawer"
+      ></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Mascafe</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer">
+      <v-list density="compact">
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :value="item"
+          active-color="primary"
+          :to="item.to"
+          router
+        >
+          <!-- <template v-slot:prepend>
+            <v-icon :icon="item.icon"></v-icon>
+          </template> -->
+          <v-list-item-title v-text="item.title"></v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script >
+  // import DefaultView from './View.vue';
+  export default {
+  data: () => ({
+    drawer: false,
+    group: null,
+    items: [
+      {
+        title: "Home",
+        value: "foo",
+        to: "/",
+      },
+      {
+        title: "Stock",
+        value: "foo",
+        to: "/stock",
+      },
+      {
+        title: "Bar",
+        value: "bar",
+        to: "/about",
+      },
+      {
+        title: "Login",
+        value: "fizz",
+        to: "/login",
+      },
+      {
+        title: "Buzz",
+        value: "buzz",
+      },
+    ],
+  }),
+
+  watch: {
+    group() {
+      this.drawer = false;
+    },
+  },
+};
+</script>
---
+++ src/views/StockView.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Stockxxxxxxxxxx</h1>
+</template>
+
+<script >
+
+</script>
