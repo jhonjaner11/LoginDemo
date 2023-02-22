0d236a649ff2b1be3e13738d4283d9dfb1cf365c
jhon janer <jhonjaner11@gmail.com>
2023-02-22 12:02:17 -0500

    add github pages

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/LoginDemo/',
+  base: '/LoginDemo/',
   plugins: [
     vue({
       template: { transformAssetUrls }
